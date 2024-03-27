11cced0b2288b03a662cb4dec07760c53276aa9b
Soofiya Binth Shajahan B <soofiyashajahan@gmail.com>
2024-03-27 09:17:49 +0530

    second commit

--- nodeCsvProcessor.js
+++ nodeCsvProcessor.js
@@ -1,23 +1,31 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.readFile('demo.csv', 'utf8', (err, data) => {
+fs.readFile("demo.csv", "utf8", (err, data) => {
   if (err) {
-    console.error('Error reading the file:', err);
+    console.error("Error reading the file:", err);
     return;
   }
 
-  const lines = data.trim().split('\n');
+  const lines = data.trim().split("\n");
 
-  lines.forEach((line, index) => {
-    const fileName = `output_${index + 1}.csv`;
-    const content = line.trim();
+  const output = setTimeout(() => {
+   
+    lines.forEach((line, index) => {
+      if (index % 2 == 1) {
+        const fileName = `output_${index}.csv`;
+        const content = line.trim();
 
-    fs.writeFile(fileName, content, (err) => {
-      if (err) {
-        console.error(`Error writing ${fileName}:`, err);
-        return;
+
+          fs.writeFile(fileName, content, (err) => {
+            if (err) {
+              console.error(`Error writing ${fileName}:`, err);
+              return;
+            }
+            console.log(`${fileName} created successfully.`);
+          });
+        
+        
       }
-      console.log(`${fileName} created successfully.`);
-    });
+    }, 2000);
   });
 });
